2d089fcff8ea9e891e725d5dbcae0efa048cf154
Silvio <sdonlic@playnirvana.com>
2026-02-18 17:27:23 +0100

    + Rabbit 2
    Workers, ack

---
+++ RabbitMQ-2/Worker/Worker.cs
@@ -0,0 +1,34 @@
+﻿// Worker
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine(" Waiting for messages");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" -> Received : {message}");
+
+    int dotsCount = message.Split(".").Length - 1;
+    await Task.Delay(1000 * dotsCount);
+
+    Console.WriteLine($" -> Finished : {message}");
+
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync("hello", autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press <Enter> to exit");
+Console.ReadLine();

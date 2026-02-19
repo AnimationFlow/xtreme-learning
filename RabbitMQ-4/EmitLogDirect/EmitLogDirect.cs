ef4545dd0fb128e687360eed60c3d97bdaea1839
Silvio <sdonlic@playnirvana.com>
2026-02-19 12:36:55 +0100

    + Rabbit 4
    direct exchange
    clone of rabbit 3

---
+++ RabbitMQ-4/EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,33 @@
+﻿// EmitLogsDirect
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "logs",
+    type: ExchangeType.Fanout
+);
+
+string[] messages = GetMessages(args);
+
+foreach (var message in messages)
+{
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+        exchange: "logs",
+        routingKey: string.Empty,
+        body: body
+    );
+
+    Console.WriteLine($" -> Sent : {message}");
+}
+
+static string[] GetMessages(string[] args)
+{
+    return ((args.Length > 0) ? args : ["Hello World!"]);
+}
---
+++ RabbitMQ-4/ReceiveLogsDirect/ReceiveLogsDirect.cs
@@ -0,0 +1,36 @@
+﻿// ReceiveLogsDirect
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "logs",
+    type: ExchangeType.Fanout
+);
+
+QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync();
+string queueName = queueDeclareResult.QueueName;
+await channel.QueueBindAsync(queue: queueName, exchange: "logs", routingKey: string.Empty);
+
+Console.WriteLine(" Waiting for messages");
+Console.WriteLine(" Press <Enter> to exit");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" -> Received : {message}");
+
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
+
+Console.ReadLine();

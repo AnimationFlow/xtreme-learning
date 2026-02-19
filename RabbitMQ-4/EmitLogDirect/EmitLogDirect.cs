b15f5712328b22512389113ef847569f04dbee5d
Silvio <sdonlic@playnirvana.com>
2026-02-19 16:09:31 +0100

    + direct exchange
    severity selection with RoutingKey

--- RabbitMQ-4/EmitLogDirect/EmitLogDirect.cs
+++ RabbitMQ-4/EmitLogDirect/EmitLogDirect.cs
@@ -3,13 +3,24 @@
 using RabbitMQ.Client;
 using System.Text;
 
+if (args.Length < 1 || args[0] is not ("info" or "warning" or "error"))
+{
+    var filename = Path.GetFileName(Environment.ProcessPath);
+    Console.Error.WriteLine($"Usage: {filename} [info|warning|error] [message...]");
+
+    Environment.ExitCode = 1;
+    return;
+}
+
+var severity = args[0];
+
 var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
 await channel.ExchangeDeclareAsync(
-    exchange: "logs",
-    type: ExchangeType.Fanout
+    exchange: "direct_logs",
+    type: ExchangeType.Direct
 );
 
 string[] messages = GetMessages(args);
@@ -19,15 +30,15 @@ foreach (var message in messages)
     var body = Encoding.UTF8.GetBytes(message);
 
     await channel.BasicPublishAsync(
-        exchange: "logs",
-        routingKey: string.Empty,
+        exchange: "direct_logs",
+        routingKey: severity,
         body: body
     );
 
-    Console.WriteLine($" -> Sent : {message}");
+    Console.WriteLine($" -> Sent : {severity} - {message}");
 }
 
 static string[] GetMessages(string[] args)
 {
-    return ((args.Length > 0) ? args : ["Hello World!"]);
+    return (args.Length > 1) ? args.Skip(1).ToArray() : ["Hello World!"];
 }
--- RabbitMQ-4/ReceiveLogsDirect/ReceiveLogsDirect.cs
+++ RabbitMQ-4/ReceiveLogsDirect/ReceiveLogsDirect.cs
@@ -4,20 +4,41 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
+if (args.Length == 0)
+{
+    args = ["info", "warning", "error"];
+}
+else if (args.Any(arg => arg is not ("info" or "warning" or "error")))
+{
+    var filename = Path.GetFileName(Environment.ProcessPath);
+    Console.Error.WriteLine($"Usage: {filename} [info|warning|error] [message...]");
+
+    Environment.ExitCode = 1;
+    return;
+}
+
 var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
 await channel.ExchangeDeclareAsync(
-    exchange: "logs",
-    type: ExchangeType.Fanout
+    exchange: "direct_logs",
+    type: ExchangeType.Direct
 );
 
-QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync();
+var queueDeclareResult = await channel.QueueDeclareAsync();
 string queueName = queueDeclareResult.QueueName;
-await channel.QueueBindAsync(queue: queueName, exchange: "logs", routingKey: string.Empty);
 
-Console.WriteLine(" Waiting for messages");
+foreach (var severity in args)
+{
+    await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "direct_logs",
+        routingKey: severity
+    );
+}
+
+Console.WriteLine($" Waiting for messages : {string.Join(", ", args)}");
 Console.WriteLine(" Press <Enter> to exit");
 
 var consumer = new AsyncEventingBasicConsumer(channel);
@@ -26,7 +47,7 @@ consumer.ReceivedAsync += (model, ea) =>
 {
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
-    Console.WriteLine($" -> Received : {message}");
+    Console.WriteLine($" -> Received : {ea.RoutingKey} - {message}");
 
     return Task.CompletedTask;
 };

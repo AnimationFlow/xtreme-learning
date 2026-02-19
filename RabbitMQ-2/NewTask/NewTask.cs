ebfd5dac9a428e7bdf787998789544ca8fdc3ac9
Silvio <sdonlic@playnirvana.com>
2026-02-19 10:12:28 +0100

    + persistent messages

--- RabbitMQ-2/NewTask/NewTask.cs
+++ RabbitMQ-2/NewTask/NewTask.cs
@@ -10,12 +10,20 @@ using var channel = await connection.CreateChannelAsync();
 await channel.QueueDeclareAsync(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
 string[] messages = GetMessages(args);
+var properties = new BasicProperties { Persistent = true };
 
 foreach (var message in messages)
 {
     var body = Encoding.UTF8.GetBytes(message);
 
-    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "task_queue", body: body);
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "task_queue",
+        mandatory: true,
+        basicProperties: properties,
+        body: body
+    );
+
     Console.WriteLine($" -> Sent : {message}");
 }
 

a3ec2f8edf33f15acf44b99a6664809c3d252e74
Silvio <sdonlic@playnirvana.com>
2026-02-20 12:38:55 +0100

    + RabbitMQ 6
    RPC
    clone from RMQ 2

---
+++ RabbitMQ-6/RPCServer/RPCServer.cs
@@ -0,0 +1,42 @@
+﻿// Worker
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "task_queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" Waiting for messages");
+Console.WriteLine(" Press <Enter> to exit");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" -> Received : {message}");
+
+    int dotsCount = message.Split(".").Length - 1;
+    await Task.Delay(1000 * dotsCount);
+
+    Console.WriteLine($" -> Finished : {message}");
+
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync("task_queue", autoAck: false, consumer: consumer);
+
+Console.ReadLine();

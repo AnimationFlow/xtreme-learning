dd62c1be19df2d4fd3f6b38ef41c38e11bbaab69
Silvio <sdonlic@playnirvana.com>
2026-02-20 10:06:23 +0100

    + RabbitMQ 5
    topic exchange
    clone from RMQ 4

---
+++ RabbitMQ-5/EmitLogTopic/EmitLogTopic.cs
@@ -0,0 +1,44 @@
+﻿// Emit Logs - Topic
+
+using RabbitMQ.Client;
+using System.Text;
+
+if (args.Length < 1 || args[0] is not ("info" or "warning" or "error"))
+{
+    var filename = Path.GetFileName(Environment.ProcessPath);
+    Console.Error.WriteLine($"Usage: {filename} [info|warning|error] [message...]");
+
+    Environment.ExitCode = 1;
+    return;
+}
+
+var severity = args[0];
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "direct_logs",
+    type: ExchangeType.Direct
+);
+
+string[] messages = GetMessages(args);
+
+foreach (var message in messages)
+{
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+        exchange: "direct_logs",
+        routingKey: severity,
+        body: body
+    );
+
+    Console.WriteLine($" -> Sent : {severity} - {message}");
+}
+
+static string[] GetMessages(string[] args)
+{
+    return (args.Length > 1) ? args.Skip(1).ToArray() : ["Hello World!"];
+}
---
+++ RabbitMQ-5/ReceiveLogsTopic/ReceiveLogsTopic.cs
@@ -0,0 +1,57 @@
+﻿// Receive Logs - Topic
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+if (args.Length == 0)
+{
+    args = ["info", "warning", "error"];
+}
+else if (args.Any(arg => arg is not ("info" or "warning" or "error")))
+{
+    var filename = Path.GetFileName(Environment.ProcessPath);
+    Console.Error.WriteLine($"Usage: {filename} [info|warning|error] [message...]");
+
+    Environment.ExitCode = 1;
+    return;
+}
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "direct_logs",
+    type: ExchangeType.Direct
+);
+
+var queueDeclareResult = await channel.QueueDeclareAsync();
+string queueName = queueDeclareResult.QueueName;
+
+foreach (var severity in args)
+{
+    await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "direct_logs",
+        routingKey: severity
+    );
+}
+
+Console.WriteLine($" Waiting for messages : {string.Join(", ", args)}");
+Console.WriteLine(" Press <Enter> to exit");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" -> Received : {ea.RoutingKey} - {message}");
+
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
+
+Console.ReadLine();

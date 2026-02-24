d4a053ce7e2a6f953819fd036cfc359c671c5bc0
Silvio <sdonlic@playnirvana.com>
2026-02-24 11:38:48 +0100

    RabbitMQ - streams hello world

---
+++ RabbitMQ-8-streams/Send/Send.cs
@@ -0,0 +1,23 @@
+﻿// RabbitMQ-8-streams/Send/Send.cs
+
+using System.Text;
+using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
+
+var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
+
+await streamSystem.CreateStream(new StreamSpec("hello-stream")
+{
+    MaxLengthBytes = 5_000_000_000
+});
+
+
+var producer = await Producer.Create(new ProducerConfig(streamSystem, "hello-stream"));
+
+await producer.Send(new Message(Encoding.UTF8.GetBytes($"Hello, World 3")));
+Console.WriteLine(" [x] Sent 'Hello, World 3'");
+
+Console.WriteLine(" [x] Press any key to exit");
+Console.ReadKey();
+await producer.Close();
+await streamSystem.Close();

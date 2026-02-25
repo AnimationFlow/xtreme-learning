e685d71b8727f98ee43a67092eeae43cccdbc923
Silvio <sdonlic@playnirvana.com>
2026-02-25 13:01:07 +0100

    webapi init

---
+++ Orleans-1-URL-shortener/Orleans-1-URL-shortener/Orleans-1-URL-shortener.cs
@@ -0,0 +1,53 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-10, 45),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/", () => 
+{
+    var urls = new[] { 
+        "/weatherforecast", 
+        "/openapi/v1.json"
+    };
+    var links = urls.Select(endpoint => $"<a href=\"{endpoint}\">{endpoint}</a>").ToList();
+    var html = $"<html><body><h1>Available Endpoints</h1><ul><li>{string.Join("</li><li>", links)}</li></ul></body></html>";
+    return Results.Content(html, "text/html");
+})
+.WithName("Home");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
